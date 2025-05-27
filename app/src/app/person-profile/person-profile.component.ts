a25ae841f39f852b01820bc41fbba61577e266c6
Angel <acherfun99@gmail.com>
2025-05-27 18:02:36 -0400

    adding person profile file

---
+++ app/src/app/person-profile/person-profile.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector:'person-profile',
+    templateUrl:'./person-profile.component.html',
+    styleUrl:'./person-profile.component.scss',
+    standalone:true,
+    imports:[]
+})
+
+export class PersonProfileComponent implements OnInit{
+
+    constructor(){
+
+    }
+
+    ngOnInit(): void {
+        
+    }
+}
